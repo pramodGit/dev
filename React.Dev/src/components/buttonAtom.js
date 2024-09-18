58c43ce2024ef463a01ea5ad8889b5b3e270f84d
pramodGit <pramodkumar.email@gmail.com>
2024-09-18 13:06:17 +0530

    Add files via upload

--- React.Dev/src/components/buttonAtom.js
+++ React.Dev/src/components/buttonAtom.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const ButtonAtom = ({ label, bgColor, onClick }) => {
+class ButtonAtom extends React.Component { 
+  componentDidMount() {
+    console.log("Button Component 1 => componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("Button Component 2 => componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("Button Component 3 => componentWillUnmount");
+  }
+  render() { 
+    return (
+      <button onClick={this.props.onClick}>{this.props.label}</button>
+    )
+  }
+}
+/*const ButtonAtom = ({ label, bgColor, onClick }) => {
   return <button onClick={onClick}>{label}</button>;
-};
+};*/
 
 export default ButtonAtom;
