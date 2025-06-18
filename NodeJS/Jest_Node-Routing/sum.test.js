a6212c7f8a59f945070af9d5562702343ce592f6
pramodGit <pramodkumar.email@gmail.com>
2025-06-18 23:55:22 +0530

    Delete NodeJS/Jest_Node-Routing directory

--- NodeJS/Jest_Node-Routing/sum.test.js
+++
@@ -1,8 +0,0 @@
-const sum = require('./sum');
-const {mult, multi} = require('./mult');
-
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
-	expect(mult(3, 2)).toBe(6);
-	expect(multi(3, 2, 2)).toBe(12);
-});
